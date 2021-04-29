1a7b6c44f68639f0a3857cfa3631fa73536a35f7
Tanweer Anwar <tanweer.anwar919@gmail.com>
2021-04-29 20:55:38 +0530

    Initial commit

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Covid Seva</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
