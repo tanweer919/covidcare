a76f867c4403ac46008cdddbaced421f5af9a67b
Tanweer Anwar <tanweer.anwar919@gmail.com>
2021-04-30 19:50:51 +0530

    Added searchbox for location search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import Layout from "../src/components/Layout";
+import SearchBox from "../src/components/SearchBox"
 const Home = (): JSX.Element => {
   return (
     <Layout selectedKey={0}>
       <section>
         <div className="flex md:hidden logo-small-box">
-          <img className="logo-small" src="/images/logo_large.png" alt="logo"/>
+          <img className="logo-small" src="/images/logo_large.png" alt="logo" />
+        </div>
+        <div>
+          <div className="grid grid-cols1">
+            <SearchBox/>
+          </div>
         </div>
       </section>
     </Layout>
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,24 +1,42 @@
-const SearchBox = (): JSX.Element => {
-  return (
-    // <div
-    //   className={`navbar__right__search-box col-11${
-    //     focus ? " navbar__right__search-box--active" : ""
-    //   }`}
-    // >
-    //   <FontAwesomeIcon
-    //     icon={faSearch}
-    //     className="navbar__right__search-box__search-icon"
-    //   />
-    //   <input
-    //     type="text"
-    //     className="navbar__right__search-box__input"
-    //     placeholder="Search"
-    //     onFocus={() => {}}
-    //     onBlur={() => {}}
-    //   />
-    // </div>
-    <div></div>
-  );
-}
+import { useState } from "react";
 
-export default SearchBox
+const SearchBox = (): JSX.Element => {
+  const [focus, setFocus] = useState(false);
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setFocus(true);
+  };
+
+  const handleFocusOut = (e: React.FocusEvent<HTMLInputElement>) => {
+    setFocus(false);
+  };
+
+  const [location, setLocation] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <div className="searchbox-container">
+      <div className={`search-box${focus ? " search-box--active" : ""}`}>
+        <img src="/images/search.svg" className="search-box__search-icon" />
+        <input
+          type="text"
+          className="search-box__input"
+          placeholder="Search for location"
+          onFocus={handleFocus}
+          onBlur={handleFocusOut}
+          onChange={handleChange}
+          name="location"
+        />
+      </div>
+      {/* <div className="autocomplete-container">
+        {matchingCity.map((city, i) => (
+          <div className="autocomplete-item" key={i}>
+            {city}
+          </div>
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default SearchBox;
