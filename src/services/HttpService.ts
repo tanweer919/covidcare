dfd4f4a7246c0954e2d60073b4e92e3d11a55b1d
Tanweer Anwar <tanweer.anwar919@gmail.com>
2021-05-16 11:18:27 +0530

    Modified http service base url

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -4,7 +4,7 @@ export default class HttpService {
   
   static getHttpClient = () => {
     let httpClient = axios.create({
-      baseURL: `${process.env.NEXT_SERVER_BASE_URL}/api`,
+      baseURL: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api`,
     });
     return httpClient;
   };
