3fe1a4a375d89a1c3bd38420a512d6df46673744
Tanweer Anwar <tanweer.anwar919@gmail.com>
2021-05-26 22:07:19 +0530

    Modified search box

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title>Covid Care</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
       <Component {...pageProps} />
